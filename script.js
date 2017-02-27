5607c1ac1125e2b2465d43909418f7d88f0f6a82
soliniak <soliniak@gmail.com>
2017-02-27 15:31:38 +0100

    Open-up

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
-	$(".simple-button").mousedown(function(){
+	$(".simple-button").on("mousedown", function(){
 		$(this).addClass("click-anim-button");
-	});	
-	$(".simple-button").on('mouseup mouseout', function(){
+	}).on('mouseup mouseout', function(){
 		$(this).removeClass("click-anim-button");
 	});
+
+	$(".open-up").on("click", function(){
+		$(".header-footer-contact").toggle("header-footer-contact-click");
+		$(".header-footer").toggle("header-footer-click");
+	});
+
 });	
